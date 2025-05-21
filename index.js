6c52584a75678a5646abcf2ab354b7c46033fd79
maurice diong <mdiong00@gmail.com>
2025-05-21 17:06:32 -0400

    lab completed. use of Math object

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random;
+random = Math.ceil(Math.random()*10)
+
+const num3 = 10;
+const num4 = 6;
+let mod = modFunction()
+function modFunction(){
+    return num3%num4
+}
+
+let max = Math.max(1,2,3,20)
